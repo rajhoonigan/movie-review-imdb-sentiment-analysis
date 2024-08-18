1a0312a19f1951bd6ff1df219b9a5b3addefca0b
rajhoonigan <45555053+rajhoonigan@users.noreply.github.com>
2024-08-18 00:09:19 +0000

    fix disutil error

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,6 +11,9 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
 import matplotlib.pyplot as plt
 import seaborn as sns
+import distutils.core
+
+print("distutils is available!")
 
 # Download NLTK data
 nltk.download('stopwords')
